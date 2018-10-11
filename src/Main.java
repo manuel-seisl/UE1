a900ec35796fa667bd3b97568c4bd46e5310bffc
Manuel Seisl <info@manuelseisl.at>
2018-10-11 11:20:06 +0200

    firstname, lastname, ageyear

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String firstname = "Manuel";
         String lastname = "Seisl";
+        int age = 26;
         int ageyear = 1992;
+        String town = "St. Ulrich am Pillersee";
 
-        System.out.println("Hello " + lastname + "!\nDein Jahrgang ist " + ageyear + "!");
+        System.out.println("Hello " + firstname + " " + lastname + "!\nDein Jahrgang ist " + ageyear + "!");
 
     }
 
