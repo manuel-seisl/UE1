382e66137809a5a56ec292023d8e33b52276f0dd
Manuel Seisl <info@manuelseisl.at>
2018-10-11 10:53:49 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String firstname = "Manuel";
+        int age = (int) 25.6;
+
+        System.out.println("Hello " + firstname + "!\nDu bist " + age + " Jahre alt!");
+
+    }
+
+}
